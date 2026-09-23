45fba674a3a3d97cf51dbeb05e1630869438657d
Mario Oliveira <Mario.Oliveira.T0133601@edu.atec.pt>
2026-09-23 18:09:53 +0100

    ASPEC-11 #done Atualização da migration de Users para incluir softdeletes e implementação do método de anonimização de dados no Model de User

--- Aspec_Backend/app/Models/User.php
+++ Aspec_Backend/app/Models/User.php
@@ -9,13 +9,15 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, softDeletes, HasUuids;
 
     /**
      * Get the attributes that should be cast.
@@ -28,5 +30,62 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    protected $fillable = [
+        'email',
+        'password',
+        'phone',
+        'role_id',
+        'account_status_id',
+        'trial_ends_at',
+    ];
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function accountStatus()
+    {
+        return $this->belongsTo(AccountStatus::class);
+    }
+
+    public function member_profile()
+    {
+        return $this->hasOne(MemberProfile::class);
+    }
+
+    public function anonymizeAndDelete()
+    {
+        if (!$this->memberProfile) {
+            throw new \Exception('O utilizador não tem um perfil de membro associado.');}
+
+        DB::transaction(function () {
+
+            if ($this->memberProfile) {
+                $this->memberProfile->update([
+                    'name'                 => 'Utilizador Anónimo',
+                    'business_name'        => 'Empresa Removida',
+                    'congregation'         => 'N/A',
+                    'role_in_congregation' => 'N/A',
+                    'logo_path'            => null,
+                    'description'          => null,
+                    'website_url'          => null,
+                    'commercial_contacts'  => null,
+                    'address'              => null,
+                ]);
+                
+                $this->memberProfile->delete(); 
+            }
+
+            $this->update([
+                'email'    => 'deleted_' . Str::uuid() . '@aspec.local',
+                'phone'    => '000000000',
+                'password' => Hash::make(Str::random(32)), 
+            ]);
+
+            $this->delete();
+        });
     }
 }
